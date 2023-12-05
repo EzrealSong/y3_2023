18516f85b8782769e1b552c04a30c45c5a34f9a2
Ezreal Song <ssong56@asu.edu>
2023-12-04 23:22:59 -0700

    Test

--- Y3初赛/custom/custom_editor_icon.py
+++ Y3初赛/custom/custom_editor_icon.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134261671: {'path': 'ui/134261671', 'describe': '', 'name': u'\u5973\u79d1\u5b66\u5bb6', 'key': 134261671, 'uid': '134261671', 'size': '579X922'}}
